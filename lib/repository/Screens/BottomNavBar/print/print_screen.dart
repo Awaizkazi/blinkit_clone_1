dde118264be15900c1d0476223c2f256f82f0bcc
Awaizkazi <Owaizkazi789@gmail.com>
2025-01-20 01:03:43 +0530

    Print screen some changes of documemt img positioning

--- lib/repository/Screens/BottomNavBar/print/print_screen.dart
+++ lib/repository/Screens/BottomNavBar/print/print_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,65 +17,56 @@ class PrintScreen extends StatelessWidget {
           children: [
             Stack(
               children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 180,
-                      color: const Color(0xffF7cb45),
-                      child: Column(
+                Container(
+                  width: double.infinity,
+                  height: 180,
+                  color: const Color(0xffF7cb45),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 25),
+                      Row(
                         children: [
-                          const SizedBox(height: 25),
-                          Row(
-                            children: [
-                              const SizedBox(width: 20),
-                              UiHelper.CustomText(
-                                  text: "Blinkit in",
-                                  color: const Color(0xff000000),
-                                  fontWeight: FontWeight.bold,
-                                  fontsize: 14,
-                                  fontfamily: "bold"),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const SizedBox(width: 20),
-                              UiHelper.CustomText(
-                                text: "16 minutes",
-                                color: const Color(0xff000000),
-                                fontWeight: FontWeight.bold,
-                                fontsize: 20,
-                                fontfamily: "bold",
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const SizedBox(width: 20),
-                              UiHelper.CustomText(
-                                  text: "HOME",
-                                  color: const Color(0xff000000),
-                                  fontWeight: FontWeight.bold,
-                                  fontsize: 14,
-                                  fontfamily: "bold"),
-                              UiHelper.CustomText(
-                                text: " - Owaiz Kazi, Mumbai Maharashtra",
-                                color: const Color(0xff000000),
-                                fontWeight: FontWeight.bold,
-                                fontsize: 14,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 10),
+                          const SizedBox(width: 20),
+                          UiHelper.CustomText(
+                              text: "Blinkit in",
+                              color: const Color(0xff000000),
+                              fontWeight: FontWeight.bold,
+                              fontsize: 14,
+                              fontfamily: "bold"),
                         ],
                       ),
-                    ),
-                    Positioned(
-                      child: UiHelper.CustomImage(img: "document.png"),
-                      top: 10,
-                      bottom: 20,
-                    ),
-                  ],
+                      Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          UiHelper.CustomText(
+                            text: "16 minutes",
+                            color: const Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontsize: 20,
+                            fontfamily: "bold",
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          UiHelper.CustomText(
+                              text: "HOME",
+                              color: const Color(0xff000000),
+                              fontWeight: FontWeight.bold,
+                              fontsize: 14,
+                              fontfamily: "bold"),
+                          UiHelper.CustomText(
+                            text: " - Owaiz Kazi, Mumbai Maharashtra",
+                            color: const Color(0xff000000),
+                            fontWeight: FontWeight.bold,
+                            fontsize: 14,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
                 ),
                 const Positioned(
                   right: 10,
@@ -111,89 +103,100 @@ class PrintScreen extends StatelessWidget {
               fontsize: 14,
             ),
             const SizedBox(height: 20),
-            Container(
-              width: 361,
-              height: 163,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 1.0), //(x,y)
-                    blurRadius: 6.0,
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(height: 10),
-                  UiHelper.CustomText(
-                      text: "Documents",
-                      color: const Color(0xff000000),
-                      fontWeight: FontWeight.bold,
-                      fontsize: 14,
-                      fontfamily: "bold"),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Row(
-                      children: [
-                        UiHelper.CustomImage(img: "star.png"),
-                        UiHelper.CustomText(
-                          text: "  Price starting at rs 3/page",
-                          color: const Color(0xff9C9C9C),
-                          fontWeight: FontWeight.normal,
-                          fontsize: 14,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Row(
-                      children: [
-                        UiHelper.CustomImage(img: "star.png"),
-                        UiHelper.CustomText(
-                          text: "  Paper quality: 70 GSM",
-                          color: const Color(0xff9C9C9C),
-                          fontWeight: FontWeight.normal,
-                          fontsize: 14,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Row(
-                      children: [
-                        UiHelper.CustomImage(img: "star.png"),
-                        UiHelper.CustomText(
-                          text: "  Single side prints",
-                          color: const Color(0xff9C9C9C),
-                          fontWeight: FontWeight.normal,
-                          fontsize: 14,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      const SizedBox(width: 20),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xff27AF34),
-                        ),
-                        child: const Text(
-                          "Upload Files",
-                          style: TextStyle(color: Colors.white, fontSize: 14),
-                        ),
+            Stack(
+              children: [
+                Container(
+                  width: 361,
+                  height: 163,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 1.0), //(x,y)
+                        blurRadius: 6.0,
                       ),
                     ],
                   ),
-                ],
-              ),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10),
+                      UiHelper.CustomText(
+                          text: "Documents",
+                          color: const Color(0xff000000),
+                          fontWeight: FontWeight.bold,
+                          fontsize: 14,
+                          fontfamily: "bold"),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Row(
+                          children: [
+                            UiHelper.CustomImage(img: "star.png"),
+                            UiHelper.CustomText(
+                              text: "  Price starting at rs 3/page",
+                              color: const Color(0xff9C9C9C),
+                              fontWeight: FontWeight.normal,
+                              fontsize: 14,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Row(
+                          children: [
+                            UiHelper.CustomImage(img: "star.png"),
+                            UiHelper.CustomText(
+                              text: "  Paper quality: 70 GSM",
+                              color: const Color(0xff9C9C9C),
+                              fontWeight: FontWeight.normal,
+                              fontsize: 14,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Row(
+                          children: [
+                            UiHelper.CustomImage(img: "star.png"),
+                            UiHelper.CustomText(
+                              text: "  Single side prints",
+                              color: const Color(0xff9C9C9C),
+                              fontWeight: FontWeight.normal,
+                              fontsize: 14,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xff27AF34),
+                            ),
+                            child: const Text(
+                              "Upload Files",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 14),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  child: UiHelper.CustomImage(img: "document.png"),
+                  bottom: 40,
+                  top: 10,
+                  right: 20,
+                ),
+              ],
             ),
           ],
         ),
